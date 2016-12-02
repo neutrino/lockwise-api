0e56d669486e214239bcddd4ad3d5a89ee669c24
ranendra <neutrino@ranendra.com.np>
2016-12-02 12:29:01 +0200

    Simple form to sumbit the email content

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # coding = utf8
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'hello_world'
+@app.route('/',  methods=['GET', 'POST'])
+def index():
+  if request.method == 'POST':
+    message = "TODO!"
+    return render_template('index.html', message=message)
+  else:
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
