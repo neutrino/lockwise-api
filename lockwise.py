f565a3e5ed7319877085bdd0b4e035f3b4f7d83d
ranendra <neutrino@ranendra.com.np>
2016-12-21 16:10:05 +0200

    Rename application file and added other confs. files

--- lockwise.py
+++ lockwise.py
@@ -23,6 +23,5 @@ def check():
     # return jsonify()
 
 
-
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from lockwise import app as application
+
+if __name__ == "__main__":
+  application.run()
