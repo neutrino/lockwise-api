1ec88fc845fc851ed74fbf23c5bb844f146941ec
ranendra <neutrino@ranendra.com.np>
2016-12-21 14:34:19 +0200

    Accept json request

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 # coding = utf8
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
 
 @app.route('/',  methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
     message = "TODO!"
-    return render_template('index.html', message=message)
+    return render_template('index.html', message = message)
   else:
     return render_template('index.html')
+
+
+@app.route('/check',  methods=['POST'])
+def check():
+  try:
+    data = request.get_json()
+    app.logger.debug(data)
+    return jsonify(data)
+  except:
+    message = {'error': 'Sorry, something went wrong.'}
+    return jsonify(message), 400
+    # return jsonify()
+
+
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
